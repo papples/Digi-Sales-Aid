a05e331ebdce29566f76f3666d2f970dca6b5c8b
Andreas paphitis <andrew_paphitis@hotmail.com>
2014-05-16 04:23:06 +0100

    Update Javascript.js

--- www/js/Javascript.js
+++ www/js/Javascript.js
@@ -1,1 +1,15 @@
-/* Javascript */
+
+// include this file before your jquery-mobile script tag
+$(document).delegate('.ui-navbar ul li > a', 'click', function() {
+  //search the navbar to deactivate the active button
+  $(this).closest('.ui-navbar').find('a').removeClass('ui-btn-active');
+ 
+  //change the active tab
+  $(this).addClass('ui-btn-active');
+ 
+  //hide the siblings
+  $('#' + $(this).attr('data-href')).show().siblings('.tab-content').hide();
+ 
+  return false;
+});
+	
